11488b3f42d41e7a68a64d8fb0aaed982b407205
Yalanday <yalanday256@gmail.com>
2025-01-10 02:29:07 +0300

    refactor index.ts, testing Validation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,67 +1,13 @@
-import express, {Request, Response, NextFunction} from 'express';
-import {connectToDB} from './db/db';
-import cors from 'cors';
-//types
-import {Routes} from "./types/types";
 // routes
-import userRoutes from "./routes/user-router";
-import initRoutes from "./routes/init-router";
-
-const corsOption = {
-    origin: ['http://localhost:3000'],  // Разрешенные источники
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],               // Разрешенные HTTP-методы
-    allowedHeaders: ['Content-Type', 'Authorization'],        // Разрешенные заголовки
-    credentials: true,                                        // Разрешить отправку cookies
-    preflightContinue: false,                                  // Обработать OPTIONS-запросы по умолчанию
-    optionsSuccessStatus: 204                                  // Статус для успешных preflight-запросов
-};
-
-class AppClass {
-    app: express.Application;
-    PORT: number | string;
-
-    constructor(port: number, corsOptions: cors.CorsOptions) {
-        this.app = express();
-        this.initializeMiddleware();
-        this.setConfigCors(corsOptions);
-        this.PORT = process.env.PORT || port;
-    }
-
-    // Метод для настройки middleware
-    private initializeMiddleware = () => this.app.use(express.json());
-
-    private setConfigCors = (configurations: cors.CorsOptions) => this.app.use(cors(configurations));
-
-// Метод для запуска сервера
-    startServer = async () => {
-        try {
-            await connectToDB();
-
-            this.app.listen(this.PORT, () => {
-                console.log(`Server is running on port ${this.PORT}`);
-            });
-
-            // Обработчик ошибок - добавляем после запуска сервера
-            this.app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-                res.status(500).json({message: 'Something went wrong', error: err.message});
-            });
-
-        } catch (error) {
-            console.error('Error starting server:', error);
-            process.exit(1);  // Завершаем процесс с кодом ошибки
-        }
-    };
-
-    public autoRegisterRoutes(routes: Routes[]) {
-        routes.forEach(route => {
-            this.app[route.method](route.path, route.handler);
-        });
-    }
-}
-
-const routes: Routes[] = [...initRoutes, ...userRoutes]
+import routes from "./routes";
+//config
+import corsOption from "./config-files/cors";
+// App
+import {AppClass} from "./app";
 
 const myApp = new AppClass(3001, corsOption)
+
+
 myApp.autoRegisterRoutes(routes);
 
 myApp.startServer().catch((error) => {
--- backend/tests/app.test.ts
+++ backend/tests/app.test.ts
@@ -1,8 +1,31 @@
 
 import {calcualate} from "../src/services/user-services";
+import {Validation} from "../src/helpers/validation";
 
 describe('app', () => {
     it('num', () => {
         expect(calcualate(5, 5)).toBe(10);
     })
+})
+
+
+
+describe('Validation: ', () => {
+
+    it('email valid', ()=>{
+        expect(Validation.email('mama@mama.ru')).toBe(true);
+    })
+
+    it('email invalid', ()=>{
+        expect(Validation.email('mama@mama')).toBe(false);
+    })
+
+    it('password valid', ()=>{
+        expect(Validation.password('A@4112341a')).toBe(true);
+    })
+
+    it('password invalid', ()=>{
+        expect(Validation.password('1234')).toBe(false);
+    })
+
 })
